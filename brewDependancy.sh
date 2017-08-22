3333159a9d47afc4bfcbabf8cdc4a51c73935e9b
gabrielpora <gabriel.groener@gmail.com>
2017-08-22 14:46:40 +0200

    script with setup

---
+++ brewDependancy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+brew install fmod
+brew install glm
+brew install glfw
+brew install glew
+brew install openal
+brew install openal
+brew install dumb
+brew install alure
+brew install openal-soft
+brew install freealut
+
+echo "Go to http://www.fmod.org/download and download the Macintosh version then open it and when the FMOD Studio Mac window pops up. \n
+ Drag the FMOD Studio folder into your goinfre folder eg. /goinfre/ggroener 
+\n Then go into the FMOD Studio and open the FMOD Studio executable"
