eeecbc087d1c638d37e896f792a702172fd8f0e9
Kalilo <kalilohansmann@gmail.com>
2017-08-25 08:38:45 +0200

    Added dependacy

--- brewDependancy.sh
+++ brewDependancy.sh
@@ -11,6 +11,8 @@ brew install alure
 brew install openal-soft
 brew install freealut
 
+brew install freetype
+
 echo "Go to http://www.fmod.org/download and download the Macintosh version then open it and when the FMOD Studio Mac window pops up. \n
  Drag the FMOD Studio folder into your goinfre folder eg. /goinfre/ggroener 
 \n Then go into the FMOD Studio and open the FMOD Studio executable"
