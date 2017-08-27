46a65c59cf2cbbc13fb482edabff088a1d694ac4
Cameron DE BRUYN <cdebruyn@e4r16p2.wtc.co.za>
2017-08-27 10:20:44 +0200

    Compiling on the mac but no window

--- cdebruyn/environ/GLFW/GLFW.class.cpp
+++ cdebruyn/environ/GLFW/GLFW.class.cpp
@@ -3,7 +3,7 @@
 
 GLFW::GLFW( void ) {
 
-	glfwSetErrorCallback(error_callback);
+	//glfwSetErrorCallback(error_callback);
 
 	/* Initialize the GLFW library */
 	if (!glfwInit())
@@ -31,7 +31,7 @@ const int		GLFW::initWindow( void ) {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 	
-	_window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, glfwGetPrimaryMonitor(), NULL);
+	_window = glfwCreateWindow(WIDTH, HEIGHT, TITLE, NULL, NULL);
 	if (!_window) {
 		glfwTerminate();
 		exit(EXIT_FAILURE);
@@ -41,11 +41,11 @@ const int		GLFW::initWindow( void ) {
 
 	/* Make the current window's context current */
 	glfwMakeContextCurrent(_window);
-//	gladLoadGLLoader((GLADloadproc), glfwGetProcAddress);
+	//gladLoadGLLoader((GLADloadproc), glfwGetProcAddress);
 	glfwSwapInterval(1);
 
 	while (!glfwWindowShouldClose(_window)) {
-		glfwGetFrameBufferSize(_window, &WIDTH, &HEIGHT);
+		glfwGetFramebufferSize(_window, &WIDTH, &HEIGHT);
 		glfwSwapBuffers(_window);
 		glfwPollEvents();
 	}
@@ -59,7 +59,7 @@ const int		GLFW::exitWindow( void ) {
 
 void	GLFW::error_callback(int error, std::string descr)
 {
-    std::fprintf(stderr, "Error: %s\n", descr);
+    std::fprintf(stderr, "Error: %s\n", descr.c_str());
 }
 
 void 	GLFW::key_callback(GLFWwindow* window, int key, int scancode, \
