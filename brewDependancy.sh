077167292b24d88fcbeb1bbc8416481c6c90bb56
GabrielPora <gabriel.groener@gmail.com>
2017-09-11 09:39:14 +0200

    removed brew install not needed

--- brewDependancy.sh
+++ brewDependancy.sh
@@ -4,14 +4,8 @@ brew install cmake
 brew install glm
 brew install glfw
 brew install glew
-brew install openal
 brew install dumb
 brew install alure
 brew install openal-soft
 brew install freealut
 brew install pkg-config
-
-
-echo "Go to http://www.fmod.org/download and download the Macintosh version then open it and when the FMOD Studio Mac window pops up. \n
- Drag the FMOD Studio folder into your goinfre folder eg. /goinfre/ggroener 
-\n Then go into the FMOD Studio and open the FMOD Studio executable"
