ec0c577c7eb0d2396f9a3a8fe1a0668482462a65
GabrielPora <gabriel.groener@gmail.com>
2017-09-14 12:37:16 +0200

    added nanogui library to our project

--- gameEngine/install_dependencies.sh
+++ gameEngine/install_dependencies.sh
@@ -2,10 +2,10 @@
 
 mkdir dependencies
 cd dependencies
-git clone --recursive git@github.com:wjakob/nanogui.git
-brew install cmake xorg-dev libglu1-mesa-dev
-brew install python-dev
-mkdir build
-cd build
+git clone https://github.com/wjakob/nanogui.git --recursive .nanogui
+brew install cmake
+brew install python
+mkdir cmake-build-debug
+cd cmake-build-debug
 cmake ../
 make
