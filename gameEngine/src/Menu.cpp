047083c0ad2895d9ff9e7375139c3a7e58992f35
Cameron DE BRUYN <cdebruyn@e4r17p10.wethinkcode.co.za>
2017-10-01 01:42:46 +0200

    Numbers and program

--- gameEngine/src/Menu.cpp
+++ gameEngine/src/Menu.cpp
@@ -50,27 +50,10 @@ Menu::Menu( GLFWwindow *window, int width, int height ) {
             break;
     }
 
-    glfwSetKeyCallback(window,
-        [](GLFWwindow *, int key, int scancode, int action, int mods) {
-            keyCallbackEvent(key, scancode, action, mods);
-        }
-    );
-
-    // Game loop
-    while (!glfwWindowShouldClose(window)) {
-        // Check if any events have been activated (key pressed, mouse moved etc.) and call corresponding response functions
-        glfwPollEvents();
-
-        // glClearColor(0.2f, 0.25f, 0.3f, 1.0f);
-        // glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-    }
-
 };
 
 Menu::~Menu( void ) {
-	glfwTerminate();
+
 };
 
 Menu::Menu(Menu const &copy) {
@@ -83,17 +66,3 @@ Menu&		Menu::operator=(Menu const &copy) {
     }
 	return *this;
 };
-
-void keyCallbackEvent(int key, int scancode, int action, int mods)
-{
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        in->esc = true;
-    if (key == GLFW_KEY_SPACE && action == GLFW_PRESS)
-        in->space = true;
- 
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
-        in->esc = false;
-    if (key == GLFW_KEY_SPACE && action == GLFW_RELEASE)
-        in->space = false;
-    
-}
