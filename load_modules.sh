90335095a8f0626dc94c55ca2555d095d7d94e68
Kalilo <kalilohansmann@gmail.com>
2017-10-01 11:21:56 +0200

    Added script for cloning external repositories

---
+++ load_modules.sh
@@ -0,0 +1,14 @@
+if [ ! -d external ]; then \
+	git clone git@github.com:Kalilo/bomberman_external.git; \
+	mv bomberman_external external; \
+fi
+
+if [ ! -d resources ]; then \
+	git clone git@github.com:Kalilo/bomberman_resources.git; \
+	mv bomberman_resources resources; \
+fi
+
+if [ ! -d SoundEngine/music ]; then \
+	git clone git@github.com:Kalilo/bomberman_music.git; \
+	mv bomberman_music SoundEngine/music; \
+fi
